1b24a35ff679b24c29fced7072560ecf65154980
Marc Wang <mwang@student.42.fr>
2015-11-16 00:17:58 +0100

    Added 2 skills (Frost Nova & Fire Pillar)

---
+++ Assets/Scripts/Casts/CastFirePillar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastFirePillar : Cast {
+
+	private AudioSource ausou;
+
+	// Use this for initialization
+	void Start () {
+		ausou = GetComponent<AudioSource>();
+		Destroy(gameObject, destroyTime + 2);
+		StartCoroutine(fireTime());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Enemy")
+			other.gameObject.GetComponent<Enemy>().hp -= damage;
+	}
+
+	IEnumerator fireTime()
+	{
+		yield return new WaitForSeconds(destroyTime);
+		GetComponent<ParticleSystem>().Stop();
+		transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
+		StartCoroutine(fadeOut());
+	}
+
+	IEnumerator fadeOut()
+	{
+		for (int i = 9; i >= 0; i--)
+		{
+			ausou.volume = i * 0.1f;
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+}
---
+++ Assets/Scripts/Casts/CastFrostNova.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastFrostNova : Cast {
+	
+	private AudioSource ausou;
+	
+	// Use this for initialization
+	void Start () {
+		ausou = GetComponent<AudioSource>();
+		Destroy(gameObject, destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Enemy")
+			other.gameObject.GetComponent<Enemy>().hp -= damage;
+	}
+}
