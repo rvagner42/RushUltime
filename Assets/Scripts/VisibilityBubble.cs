9ee42942ea26dd771cbad3e780932d83177c337c
Marc WANG <mwang@e2r13p6.42.fr>
2015-12-04 17:22:43 +0100

    Better transparent walls

---
+++ Assets/Scripts/VisibilityBubble.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisibilityBubble : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Wall" && transform.position.z < other.transform.position.z)
+		{
+			Color alpha = new Color();
+			Material[] materials;
+			materials = other.transform.gameObject.GetComponent<Renderer>().materials;
+			foreach (Material elem in materials)
+			{
+				alpha = elem.color;
+				alpha.a = 0.1f;
+				elem.color = alpha;
+			}
+		}
+		else if (other.tag == "Wall" && transform.position.z > other.transform.position.z)
+		{
+			Color alpha = new Color();
+			Material[] materials;
+			materials = other.transform.gameObject.GetComponent<Renderer>().materials;
+			foreach (Material elem in materials)
+			{
+				alpha = elem.color;
+				alpha.a = 1.0f;
+				elem.color = alpha;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		Color alpha = new Color();
+		Material[] materials;
+		materials = other.transform.gameObject.GetComponent<Renderer>().materials;
+		foreach (Material elem in materials)
+		{
+			alpha = elem.color;
+			alpha.a = 1.0f;
+			elem.color = alpha;
+		}
+	}
+}
